7d8734e108702e85f1b76438a137759bed47d7ec
Xavier Georget <xageorget@gmail.com>
2023-02-07 17:34:21 +0100

    feature/category-section category cards with logo

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -15,7 +15,7 @@ export default function Food() {
 
     return (
         <section className="max-w-[1640px] m-auto px-4 py-12">
-            <h1 className="text-orange-600 font-bold text-4xl text-center">
+            <h1 className="text-orange-600 font-bold text-4xl text-center mb-2">
                 Top Rated Menu Items
             </h1>
             {/* Filter Row */}
